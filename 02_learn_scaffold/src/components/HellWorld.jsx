e1a1bf8eee45d7d78cbbe18d23be9e19b6c6675c
wx_orange <wx_orange@orange007.com>
2024-04-11 17:46:10 +0800

    组件学习

---
+++ 02_learn_scaffold/src/components/HellWorld.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello World</h2>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld
